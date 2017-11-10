632da9df10ff7d3293e0aedc7e02fb08d4b5a56f
Alex Notes <me@axn.io>
2017-11-10 14:33:05 +0100

    Add basic classes

---
+++ BeerBar/src/main/java/be/ecam/beerbar/Brewery.java
@@ -0,0 +1,16 @@
+package be.ecam.beerbar;
+
+public enum Brewery {
+    
+    SAINT_SIXTE("Brasserie de l'abbaye de Saint-Sixte");
+
+    private final String name;
+    
+    private Brewery(String name) {
+        this.name = name;
+    }
+    
+    public String getBreweryName() {
+        return this.name;
+    } 
+}
