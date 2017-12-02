1060985045f9ef220ff67389508bf1ba24b36e79
Alex Notes <me@axn.io>
2017-12-02 23:50:04 +0100

    Bottle: remove the id field because the pair number is more adequate

--- BeerBar/src/main/java/be/ecam/beerbar/Bottle.java
+++ BeerBar/src/main/java/be/ecam/beerbar/Bottle.java
@@ -4,12 +4,7 @@ package be.ecam.beerbar;
  * This class represents a bottle in the inventory.
  */
 public class Bottle {
-    
-    // Shared variable between all instances which purpose is to
-    // define the ID of the bottle instance in the constructor
-    private static int counter = 1;
-    
-    private final int id;
+
     private final String name;
     private final int volume;
     private final int alcRate;
@@ -17,7 +12,6 @@ public class Bottle {
     private final Brewery brewery;
     
     public Bottle(String name, int volume, int alcRate, BeerColor color, Brewery brewery) {
-        this.id      = Bottle.counter++;
         this.name    = name;
         this.volume  = volume;
         this.alcRate = alcRate;
@@ -28,7 +22,7 @@ public class Bottle {
     @Override
     public String toString() {
         // ex: 1) Jupiler, 33cl, Blonde @ 7% d'alcool de Brasserie Jupiler
-        return String.format("%d) %s, %dcl, %s @ %d%% d'alcool de %s",
-                this.id, this.name, this.volume, this.color, this.alcRate, this.brewery);
+        return String.format("%s, %dcl, %s @ %d%% d'alcool de %s",
+            this.name, this.volume, this.color, this.alcRate, this.brewery);
     }
 }
