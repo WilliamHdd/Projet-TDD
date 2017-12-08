75dad250336dd33df224b03dee7c88a59e2e16ff
Julien Kessels <julien.kessels@gmail.com>
2017-12-08 15:16:46 +0100

    first tests

--- BeerBar/src/test/java/MainTest.java
+++ BeerBar/src/test/java/MainTest.java
@@ -1,3 +1,6 @@
+import be.ecam.beerbar.BeerColor;
+import be.ecam.beerbar.Bottle;
+import be.ecam.beerbar.Brewery;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,5 +10,14 @@ public class MainTest {
     public void shouldTest() {
         Assert.assertEquals("Hello", "Hello");
     }
+
+    public void addBottleTest() {
+        Bottle bot = new Bottle("jupiler", 25, 4, BeerColor.BLONDE, Brewery.JUPILER);
+        Assert.assertEquals(bot.getName(),"jupiler");
+        Assert.assertEquals(bot.getVolume(),25);
+        Assert.assertEquals(bot.getColor(),BeerColor.BLONDE);
+    }
+
+
 }
 
